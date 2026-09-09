c62186a08d5428440085178410cc2b96f62245f4
Yamong Phoo <yamongphoo012@gmail.com>
2026-09-09 14:27:31 +0300

    update in about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
           <img src={about} className="about-photo" alt="awesome beach" />
         </div>
         <article className="about-info">
-          <h3>explore the difference</h3>
+          <h3>explore the difference Yamong</h3>
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
